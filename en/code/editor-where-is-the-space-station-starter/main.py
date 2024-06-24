44357cb1e6427a6b85c8f87bda8f0ba3b3ff9141
Laura Sach <5183697+lawsie@users.noreply.github.com>
2024-06-24 13:02:51 +0100

    Update starter doc

--- en/code/editor-where-is-the-space-station-starter/main.py
+++ en/code/editor-where-is-the-space-station-starter/main.py
@@ -1,1 +1,9 @@
-print("hi")
+import json
+import turtle
+import urllib.request
+
+url = "https://corquaid.github.io/international-space-station-APIs/JSON/people-in-space.json"
+
+response = urllib.request.urlopen(url)
+astros = json.loads(response.read())
+print(astros)
