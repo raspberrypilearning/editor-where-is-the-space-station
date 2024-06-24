74a91f2bf4b8b83a5b55e5d158210d291beecb17
Laura Sach <5183697+lawsie@users.noreply.github.com>
2024-06-24 13:04:44 +0100

    Wrong!

--- en/code/editor-where-is-the-space-station-starter/main.py
+++ en/code/editor-where-is-the-space-station-starter/main.py
@@ -1,9 +1,8 @@
-import json
 import turtle
-import urllib.request
+import requests
 
 url = "https://corquaid.github.io/international-space-station-APIs/JSON/people-in-space.json"
 
-response = urllib.request.urlopen(url)
-astros = json.loads(response.read())
-print(astros)
+r = requests.get(url)
+data = r.json()
+print(data)
